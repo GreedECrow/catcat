45d7e744387ed3e9f587f7d55a3795c208333157
Lee Pierson <awesomehenge@gmail.com>
2023-08-03 17:08:25 +0100

    update04

---
+++ client/src/componants/BookCard/BookCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./BookCard.css";
+import Form from '../Form/Form';
+
+export default function BookCard({book, handleDelete, HandleBook}) {
+
+    return(
+        <div>
+            {book.map(book => {
+                return (
+                    <div key={book._id}>
+                        <div>
+                            <p onClick={() => handleDelete(book._id)}>X</p>
+                            <h3>{book.name}</h3>
+                            <img src={book.img_url} />
+                            <p>{book.description}, {book.status}</p>
+                        </div>
+                    <Form onSubmitFunc={handleUpdateBook} book={book}/>
+                <div />
+                )
+            })}
+        </div>
+    )
+}
--- client/src/componants/Form/Form.js
+++ client/src/componants/Form/Form.js
@@ -7,10 +7,25 @@ export default function Form({onSubmitFunc, movie}) {
         description: "",
         status: "",
     })
-}
 
 const handleChange = (event) => {
     setFormData({...formData, [event.target.name] : event.target.value});
     console.log(formData);
 }
 
+const submit = event => {
+    event.preventDefault();
+    onSubmitFunc(formData);
+}
+
+return(
+    <div>
+        <firm onSubmit={submit}>
+            <input placeHolder="book title" onChange={handleChange} name="title" value={formData.title}></input>
+            <input placeHolder="img url" onChange={handleChange} name="img_url" value={formData.img_url}></input>
+            <input placeHolder="description" onChange={handleChange} name="description" value={formData.description}></input>
+            <input placeHolder="status" onChange={handleChange} name="status" value={formData.status}></input>
+        </firm>
+    </div>
+)
+}
